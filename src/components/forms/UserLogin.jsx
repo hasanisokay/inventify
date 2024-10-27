b3d4e5234988d158ec2c5de439a16b1efe54bab3
hasanisokay <hasanisokay@gmail.com>
2024-10-28 02:54:26 +0600

    a

--- src/components/forms/UserLogin.jsx
+++ src/components/forms/UserLogin.jsx
@@ -22,13 +22,16 @@ const UserLogin = () => {
             return;
         }
 
-
+        const formData = {
+            username: username,
+            password: password
+        }
         const res = await fetch(`/api/login`, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ username, password }),
+            body: JSON.stringify(formData),
         });
 
         const data = await res.json();
