a37e259866fd0b7ec176e0c8f59e5c71e2e64a94
hasanisokay <hasanisokay@gmail.com>
2024-11-21 11:41:13 +0600

    to the cdn of tailwind in print css

--- src/components/modal/PrintInvoiceModal.jsx
+++ src/components/modal/PrintInvoiceModal.jsx
@@ -15,9 +15,10 @@ const PrintInvoiceModal = ({ resetStates, openModal, setOpenModal, invoiceNumber
         const printWindow = window.open('', '', 'height=600,width=800');
         printWindow.document.write(`<html><head><title>${invoiceNumber}</title>`);
 
-        // printWindow.document.write('<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">');
+        printWindow.document.write('<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">');
+        // printWindow.document.write('<link href="./tailwind.min.css" rel="stylesheet">');
+        // printWindow.document.write('<link href="./../../../public/css/tailwind.min.css" rel="stylesheet">');
         // printWindow.document.write('<link href="/public/css/tailwind.min.css" rel="stylesheet">');
-        printWindow.document.write('<link href="./../../../public/css/tailwind.min.css" rel="stylesheet">');
         printWindow.document.write('<link href="./../../app/globals.css" rel="stylesheet">');
 
         printWindow.document.write('</head><body>');
